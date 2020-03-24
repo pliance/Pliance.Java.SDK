bc9e8e1816b827cec71ec947d68e129c189a86d1
Adam Furtenbach <adam.furtenbach@gmail.com>
2020-03-24 10:38:18 +0100

    classifyCompanyHit

--- src/test/java/com/pliance/app/TestBase.java
+++ src/test/java/com/pliance/app/TestBase.java
@@ -1,5 +1,6 @@
 package com.pliance.app;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.util.UUID;
 import junit.framework.TestCase;
@@ -17,10 +18,17 @@ public abstract class TestBase extends TestCase {
 	protected String _referenceId;
 
 	protected TestBase() {
+		FileInputStream file = null;
+
+		try {
+			file = new FileInputStream(new File("client.pfx"));
+		} catch (Exception e) {
+		}
+
 		_secret = "2bb80d537b1da3e38bd30361aa855686bde0eacd7162fef6a25fe97bf527a25b";
 		_issuer = "Demo";
 		_url = "https://local.pliance.io/";
-		_certificate = null;
+		_certificate = file;
 		_factory = createFactory();
 		_client = _factory.create("Adam", "42");
 		_referenceId = UUID.randomUUID().toString();
